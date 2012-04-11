e367d9255a1a41bef446ddef92cfd5e9f03ae554
Juan Ramón <juanramon.diaz@gmail.com>
2012-04-11 10:08:08 +0200

    NewCorp v2.3

--- newcorp/index.php
+++ newcorp/index.php
@@ -3,7 +3,7 @@
     function newcorp_theme_info() {
         $theme = array(
             'name'         => 'NewCorp'
-            ,'version'     => '2.2'
+            ,'version'     => '2.3'
             ,'description' => 'This is the OSClass theme for a job board.'
             ,'author_name' => 'OSClass Team'
             ,'author_url'  => 'http://osclass.org'
