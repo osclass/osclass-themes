d4ef02bfd9d6b669aafdec67ee7a7051238f7a6e
Juan Ramón <juanramon.diaz@gmail.com>
2012-05-29 11:48:41 +0200

    compatibility with < v2.4

--- realestate/functions.php
+++ realestate/functions.php
@@ -20,7 +20,7 @@
      */
     //Preferences
     if(!osc_get_preference('keyword_placeholder','realestate')){
-        osc_set_preference('keyword_placeholder',__('Luxury Villas'),'realestate');
+        osc_set_preference('keyword_placeholder',__('Luxury Villas', 'realestate'),'realestate');
     }
     function item_realestate_attributes(){
         //get_realestate_attributes
@@ -216,6 +216,180 @@
     }
     </script>
     <?php
+        }
+    }
+
+    // hacks to work with < 2.4 versions
+    if( !defined('OC_ADMIN') ) {
+        define('OC_ADMIN', false) ;
+    }
+
+    if( !function_exists('meta_title') ) {
+        function meta_title( ) {
+            $location = Rewrite::newInstance()->get_location();
+            $section  = Rewrite::newInstance()->get_section();
+
+            switch ($location) {
+                case ('item'):
+                    switch ($section) {
+                        case 'item_add':    $text = __('Publish an item', 'realestate') . ' - ' . osc_page_title(); break;
+                        case 'item_edit':   $text = __('Edit your item', 'realestate') . ' - ' . osc_page_title(); break;
+                        case 'send_friend': $text = __('Send to a friend', 'realestate') . ' - ' . osc_item_title() . ' - ' . osc_page_title(); break;
+                        case 'contact':     $text = __('Contact seller', 'realestate') . ' - ' . osc_item_title() . ' - ' . osc_page_title(); break;
+                        default:            $text = osc_item_title() . ' - ' . osc_page_title(); break;
+                    }
+                break;
+                case('page'):
+                    $text = osc_static_page_title() . ' - ' . osc_page_title();
+                break;
+                case('error'):
+                    $text = __('Error', 'realestate') . ' - ' . osc_page_title();
+                break;
+                case('search'):
+                    $region   = Params::getParam('sRegion');
+                    $city     = Params::getParam('sCity');
+                    $pattern  = Params::getParam('sPattern');
+                    $category = osc_search_category_id();
+                    $category = ((count($category) == 1) ? $category[0] : '');
+                    $s_page   = '';
+                    $i_page   = Params::getParam('iPage');
+
+                    if($i_page != '' && $i_page > 0) {
+                        $s_page = __('page', 'realestate') . ' ' . ($i_page + 1) . ' - ';
+                    }
+
+                    $b_show_all = ($region == '' && $city == '' & $pattern == '' && $category == '');
+                    $b_category = ($category != '');
+                    $b_pattern  = ($pattern != '');
+                    $b_city     = ($city != '');
+                    $b_region   = ($region != '');
+
+                    if($b_show_all) {
+                        $text = __('Show all items', 'realestate') . ' - ' . $s_page . osc_page_title();
+                    }
+
+                    $result = '';
+                    if($b_pattern) {
+                        $result .= $pattern . ' &raquo; ';
+                    }
+
+                    if($b_category) {
+                        $list        = array();
+                        $aCategories = Category::newInstance()->toRootTree($category);
+                        if(count($aCategories) > 0) {
+                            foreach ($aCategories as $single) {
+                                $list[] = $single['s_name'];
+                            }
+                            $result .= implode(' &raquo; ', $list) . ' &raquo; ';
+                        }
+                    }
+
+                    if($b_city) {
+                        $result .= $city . ' &raquo; ';
+                    }
+
+                    if($b_region) {
+                        $result .= $region . ' &raquo; ';
+                    }
+
+                    $result = preg_replace('|\s?&raquo;\s$|', '', $result);
+
+                    if($result == '') {
+                        $result = __('Search', 'realestate');
+                    }
+
+                    $text = $result . ' - ' . $s_page . osc_page_title();
+                break;
+                case('login'):
+                    switch ($section) {
+                        case('recover'): $text = __('Recover your password', 'realestate') . ' - ' . osc_page_title();
+                        default:         $text = __('Login', 'realestate') . ' - ' . osc_page_title();
+                    }
+                break;
+                case('register'):
+                    $text = __('Create a new account', 'realestate') . ' - ' . osc_page_title();
+                break;
+                case('user'):
+                    switch ($section) {
+                        case('dashboard'):       $text = __('Dashboard', 'realestate') . ' - ' . osc_page_title(); break;
+                        case('items'):           $text = __('Manage my items', 'realestate') . ' - ' . osc_page_title(); break;
+                        case('alerts'):          $text = __('Manage my alerts', 'realestate') . ' - ' . osc_page_title(); break;
+                        case('profile'):         $text = __('Update my profile', 'realestate') . ' - ' . osc_page_title(); break;
+                        case('change_email'):    $text = __('Change my email', 'realestate') . ' - ' . osc_page_title(); break;
+                        case('change_password'): $text = __('Change my password', 'realestate') . ' - ' . osc_page_title(); break;
+                        case('forgot'):          $text = __('Recover my password', 'realestate') . ' - ' . osc_page_title(); break;
+                        default:                 $text = osc_page_title(); break;
+                    }
+                break;
+                case('contact'):
+                    $text = __('Contact', 'realestate') . ' - ' . osc_page_title();
+                break;
+                default:
+                    $text = osc_page_title();
+                break;
+            }
+
+            $text = str_replace("\n", '', $text) ;
+            $text = trim($text) ;
+            $text = osc_esc_html($text) ;
+            return (osc_apply_filter('meta_title_filter', $text)) ;
+        }
+    }
+
+    if( !function_exists('meta_description') ) {
+        function meta_description( ) {
+            $location = Rewrite::newInstance()->get_location();
+            $section  = Rewrite::newInstance()->get_section();
+            $text     = '';
+
+            switch ($location) {
+                case ('item'):
+                    switch ($section) {
+                        case 'item_add':    $text = ''; break;
+                        case 'item_edit':   $text = ''; break;
+                        case 'send_friend': $text = ''; break;
+                        case 'contact':     $text = ''; break;
+                        default:
+                            $text = osc_item_category() . ', ' . osc_highlight(strip_tags(osc_item_description()), 140) . '..., ' . osc_item_category();
+                            break;
+                    }
+                break;
+                case('page'):
+                    $text = osc_highlight(strip_tags(osc_static_page_text()), 140, '', '') ;
+                break;
+                case('search'):
+                    $result = '';
+
+                    if(osc_count_items() == 0) {
+                        $text = '';
+                    }
+
+                    if(osc_has_items ()) {
+                        $result = osc_item_category() . ', ' . osc_highlight(strip_tags(osc_item_description()), 140) . '..., ' . osc_item_category();
+                    }
+
+                    osc_reset_items();
+                    $text = $result;
+                    break;
+                case(''): // home
+                    $result = '';
+                    if(osc_count_latest_items() == 0) {
+                        $text = '';
+                    }
+
+                    if(osc_has_latest_items()) {
+                        $result = osc_item_category() . ', ' . osc_highlight(strip_tags(osc_item_description()), 140) . '..., ' . osc_item_category();
+                    }
+
+                    osc_reset_latest_items();
+                    $text = $result;
+                break;
+            }
+
+            $text = str_replace("\n", '', $text) ;
+            $text = trim($text) ;
+            $text = osc_esc_html($text) ;
+            return (osc_apply_filter('meta_description_filter', $text)) ;
         }
     }
 ?>
