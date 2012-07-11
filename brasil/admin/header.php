1f0c9111d8a773699bfa99f53cb8e2ee231c7c5c
Juan Ramón <juanramon.diaz@gmail.com>
2012-07-11 22:19:18 +0200

    minor typo fixes + regenerated i18n files in brasil theme

--- brasil/admin/header.php
+++ brasil/admin/header.php
@@ -20,7 +20,7 @@
     </p>
     <?php } else { ?>
         <div class="flashmessage flashmessage-warning flashmessage-inline" style="display: block;">
-            <p><?php _e("There isn't any logo uploaded yet", 'brasil'); ?></p>
+            <p><?php _e("No logo has been uploaded yet", 'brasil'); ?></p>
         </div>
     <?php } ?>
     <h2 class="render-title separate-top"><?php _e('Upload logo', 'brasil') ?></h2>
@@ -51,8 +51,8 @@
         <p>
             <?php
                 $msg  = sprintf(__('The images folder <strong>%s</strong> is not writable on your server', 'brasil'), WebThemes::newInstance()->getCurrentThemePath() ."images/" ) .", ";
-                $msg .= __("OSClass can't upload logo image from the administration panel.", 'brasil') . ' ';
-                $msg .= __('Please make the mentioned image folder writable.', 'brasil') . ' ';
+                $msg .= __("OSClass can't upload the logo image from the administration panel.", 'brasil') . ' ';
+                $msg .= __("Please make the aforementioned image folder writable.", 'brasil') . ' ';
                 echo $msg;
             ?>
         </p>
