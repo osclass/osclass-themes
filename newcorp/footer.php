a87118a43fce3ac3a1e9f5abb3d814d8c305319b
Fernando Alou <hola@fernandoalou.com>
2012-08-09 09:56:30 +0200

    Italia branch

--- newcorp/footer.php
+++
@@ -1,5 +0,0 @@
-<div id="footer">
-    <div class="inner">
-        <?php _e('This website is proudly using the <a title="OSClass web" href="http://osclass.org/">classifieds scripts</a> software <strong>OSClass</strong>', 'newcorp'); ?>.
-    </div>
-</div>
