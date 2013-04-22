589436d0eac473a0ad47d0782956a16b2e8a2abc
Juan Ramón <juanramon.diaz@gmail.com>
2013-04-22 19:32:12 +0200

    enqueue jquery-validate in contact + changed order of contact fields

--- bender/contact.php
+++ bender/contact.php
@@ -20,6 +20,7 @@
      */
 
     bender_add_boddy_class('contact');
+    osc_enqueue_script('jquery-validate');
     osc_current_web_theme_path('header.php');
 ?>
 <div class="form-container form-horizontal form-container-box">
@@ -28,11 +29,22 @@
     </div>
     <div class="resp-wrapper">
         <ul id="error_list"></ul>
-        <form action="<?php echo osc_base_url(true); ?>
-            " method="post" >
+        <form name="contact_form" action="<?php echo osc_base_url(true); ?>" method="post" >
             <input type="hidden" name="page" value="contact" />
             <input type="hidden" name="action" value="contact_post" />
-
+            <div class="control-group">
+                <label class="control-label" for="yourName">
+                    <?php _e('Your name', 'bender'); ?>
+                    (<?php _e('optional', 'bender'); ?>)</label>
+                <div class="controls">
+                    <?php ContactForm::your_name(); ?></div>
+            </div>
+            <div class="control-group">
+                <label class="control-label" for="yourEmail">
+                    <?php _e('Your email address', 'bender'); ?></label>
+                <div class="controls">
+                    <?php ContactForm::your_email(); ?></div>
+            </div>
             <div class="control-group">
                 <label class="control-label" for="subject">
                     <?php _e('Subject', 'bender'); ?>
@@ -45,19 +57,6 @@
                     <?php _e('Message', 'bender'); ?></label>
                 <div class="controls textarea">
                     <?php ContactForm::your_message(); ?></div>
-            </div>
-            <div class="control-group">
-                <label class="control-label" for="yourName">
-                    <?php _e('Your name', 'bender'); ?>
-                    (<?php _e('optional', 'bender'); ?>)</label>
-                <div class="controls">
-                    <?php ContactForm::your_name(); ?></div>
-            </div>
-            <div class="control-group">
-                <label class="control-label" for="yourEmail">
-                    <?php _e('Your e-mail address', 'bender'); ?></label>
-                <div class="controls">
-                    <?php ContactForm::your_email(); ?></div>
             </div>
             <div class="control-group">
                 <div class="controls">
