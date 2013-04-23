a7b2b26ede36dff4ff6a84263b0610a8be98c036
Fernando Alou <hola@fernandoalou.com>
2013-04-23 10:20:37 +0200

    404 structure and styles

--- bender/functions.php
+++ bender/functions.php
@@ -154,10 +154,10 @@ FUNCTIONS
                                 <span class="admin-options">
                                     <a href="<?php echo osc_item_edit_url(); ?>" rel="nofollow"><?php _e('Edit item', 'bender'); ?></a>
                                     <span>|</span>
-                                    <a class="delete" onclick="javascript:return confirm('<?php echo osc_esc_js(__('This action can not be undone. Are you sure you want to continue?', 'modern')); ?>')" href="<?php echo osc_item_delete_url();?>" ><?php _e('Delete', 'modern'); ?></a>
+                                    <a class="delete" onclick="javascript:return confirm('<?php echo osc_esc_js(__('This action can not be undone. Are you sure you want to continue?', 'bender')); ?>')" href="<?php echo osc_item_delete_url();?>" ><?php _e('Delete', 'bender'); ?></a>
                                     <?php if(osc_item_is_inactive()) {?>
                                     <span>|</span>
-                                    <a href="<?php echo osc_item_activate_url();?>" ><?php _e('Activate', 'modern'); ?></a>
+                                    <a href="<?php echo osc_item_activate_url();?>" ><?php _e('Activate', 'bender'); ?></a>
                                     <?php } ?>
                                 </span>
                             <?php } ?>
